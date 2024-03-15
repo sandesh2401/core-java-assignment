3bcf5f8b1644a81e10e0789bd19184f77b9b8a09
sandesh2401 <161586648+sandesh2401@users.noreply.github.com>
2024-03-15 12:39:29 +0530

    Add files via upload

--- day_03/Program4.java
+++ day_03/Program4.java
@@ -3,9 +3,11 @@
         int num1 = 10;
         float num2 = num1;
         double num3 = num1;
+        String str = Integer.toString(num1);
         System.out.println("Integer :   " +num1);
         System.out.println("Float   :   " +num2);
         System.out.println("Double  :   " +num3);
+        System.out.println("String  :   "+str);
     }
     
 }
