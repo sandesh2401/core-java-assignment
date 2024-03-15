af7a6beb6edb0d0770107b7379b1141f9fb9fcdb
sandesh2401 <161586648+sandesh2401@users.noreply.github.com>
2024-03-15 12:09:46 +0530

    Add files via upload

--- day_03/Widening.java
+++ day_03/Widening.java
@@ -1,7 +1,7 @@
 class Program {
     public static void main(String args[]) {
         int num1 = 10;
-        float num2 = num1;
+        double num2 = num1;
         System.out.println("Int to float conversion : " +num2);
         System.out.println(a);
     }
